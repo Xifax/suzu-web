05e76e556a24431f83195113ab2c5198ebb30497
unknown <Yadavito@Sevenarium.(none)>
2012-03-21 23:46:13 +0400

    another update

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#import os
 
 #from flask import Flask
 #app = Flask(__name__)
@@ -12,11 +15,43 @@ import os
     #port = int(os.environ.get('PORT', 5000))
     #app.run(host='0.0.0.0', port=port)
     
-from bottle import route, run
+#from bottle import route, run
 
-@route('/hello/:name')
-def index(name='World'):
-    return '<b>Hello %s!</b>' % name
+##@route('/hello/:name')
+#@route('/')
+#def index(name='World'):
+    #return '<b>Hello %s!</b>' % name
 
-run(host='0.0.0.0', port=5000)
-#run(host='localhost', port=8080)
+#run(host='0.0.0.0', port=5000)
+##run(host='localhost', port=8080)
+
+import os
+from os import environ as env
+from sys import argv
+
+import bottle
+from bottle import default_app, request, route, response, get
+
+bottle.debug(True)
+
+@get('/')
+def index():
+    response.content_type = 'text/plain; charset=utf-8'
+    ret = 'Hello world, I\'m %s!\n\n' % os.getpid()
+    ret += 'Request vars:\n'
+    for k, v in request.environ.iteritems():
+        if 'bottle.' in k:
+            continue
+        ret += '%s=%s\n' % (k, v)
+
+    ret += '\n'
+    ret += 'Environment vars:\n'
+
+    for k, v in env.iteritems():
+        if 'bottle.' in k:
+            continue
+        ret += '%s=%s\n' % (k, v)
+
+    return ret
+
+bottle.run(host='0.0.0.0', port=argv[1])
