2a40285ec914bb9d254886071a61ac4443d4f42c
Xifax <demi.log@gmail.com>
2012-09-30 20:11:09 +0400

    Some quickhacks regarding Weblio script. Roadmap stub.

--- src/weblio.py
+++ src/weblio.py
@@ -13,14 +13,17 @@ from bs4 import BeautifulSoup
 class Weblio:
 
     def __init__(self):
-        self.definition_url = 'http://www.weblio.jp/content/%s'
+        #self.definition_url = 'http://www.weblio.jp/content/%s'
+        # Cannot find some rare words
+        self.definition_url = 'http://ejje.weblio.jp/english-thesaurus/%s'
         self.lookup_url = 'http://ejje.weblio.jp/content/%s'
         self.examples_url = 'http://ejje.weblio.jp/sentence/content/%s'
         # TODO: stats
 
     def definition(self, term):
-        '''Fetches definitions and similar words'''
+        '''Fetches definitions and similar words, synonyms'''
         data = self.process(self.definition_url, term)
+        # TODO: implement!
         if data:
             # Check for different possible divs:
             # NetDicBody
@@ -37,11 +40,21 @@ class Weblio:
     def examples(self, term):
         '''Fetches examples'''
         data = self.process(self.examples_url, term)
+        examples = []
         if data:
-            for example in data.find_all('div', 'qotC')[:2]:
+            # Iterate from the END, not the beginning
+            for example in data.find_all('div', 'qotC')[2:]:
+                # TODO: if no examples found -> log it (and mark term)
+                # TODO: check (term:example) when there's english example [0] instead
                 print example.contents[0].getText()
                 source = example.contents[1].span.getText()
                 print example.contents[1].getText().replace(source, '')
+
+
+                # temporary solution
+                #examples.append(example.contents[0].getText())
+
+        return examples
 
     def process(self, url, term):
         try:
@@ -49,6 +62,31 @@ class Weblio:
         except RequestException:
             return None
 
+
+def local_run():
+    output = open('/home/yadavito/Desktop/test2_export.txt', 'w')
+    export = []
+    for line in open('/home/yadavito/Desktop/test2.txt'):
+        fields = line.split('\t')
+        term = fields[0]
+        weblio = Weblio()
+        examples = weblio.examples(term)
+        if examples:
+            example = weblio.examples(term).pop()
+        else:
+            example = u''
+        #print example
+        fields[0] = unicode(fields[0], 'utf-8')
+        fields[1] = unicode(fields[1], 'utf-8')
+        fields[2] = unicode(fields[2], 'utf-8')
+        fields[3] = example
+        export.append(fields)
+
+    print export
+    for line in export:
+        output.write(u'\t'.join(line).encode('utf-8'))
+        # OMG
+        output.write('\n')
 
 if __name__ == '__main__':
     '''Run as test script'''
@@ -58,7 +96,7 @@ if __name__ == '__main__':
     r = requests.get('http://ejje.weblio.jp/sentence/content/%s' % term)
     data = BeautifulSoup(r.text)
     # Two examples should be plenty | TODO: get random two indices?
-    for example in data.find_all('div', 'qotC')[:2]:
+    for example in data.find_all('div', 'qotC')[2:]:
         # Sentence
         print example.contents[0].getText()
         # Translation
@@ -66,6 +104,8 @@ if __name__ == '__main__':
         print example.contents[1].getText().replace(source, '')
     '''
 
-    weblio = Weblio()
-    weblio.definition(term)
+    #weblio = Weblio()
+    #weblio.definition(term)
     #weblio.examples(term)
+    local_run()
+
