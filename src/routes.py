b7f6d67332a6b07ef825a1e36eb89315b80460e0
Xifax <demi.log@gmail.com>
2012-10-07 20:46:17 +0400

    Minor tweaks. New actions should now work. Now on to the desktop app.

--- src/routes.py
+++ src/routes.py
@@ -104,12 +104,11 @@ def lookup_item(key):
             readings = mecab.wordByWord(example)
             results.append({
                 'example': example,
-                #'reading': reading,
                 'readings': readings,
                 'translation': translation
             })
 
-        # TODO: add another (optional) key to rout -> response type, json|html
+        # TODO: add another (optional) key to route -> response type, json|html
         #return {'term': key, 'data': results}
 
         return render('lookup', term=key, examples=results)
@@ -126,6 +125,15 @@ def add_item(key):
     else:
         return {'result': 'failure', 'reason': 'Item is already in DB'}
 
+@route('/del/:key')
+def del_item(key):
+    """Remove existing item"""
+    results = {}
+    for item in Key.objects(value=key):
+        item.delete()
+        results[key] = {'result': 'success'}
+    return results
+
 @route('/add/:key/:example')
 def add_item_with_example(key, example):
     """Add new item, generate fact and link example"""
@@ -133,7 +141,6 @@ def add_item_with_example(key, example):
         return {'result': 'success'}
     else:
         return {'result': 'failure', 'reason': 'Could not add new item'}
-    #return {'key': key, 'example': example}
 
 @route('/add/:example/<keys:path>')
 def add_example_with_items(example, keys):
@@ -142,7 +149,6 @@ def add_example_with_items(example, keys):
         return {'result': 'success'}
     else:
         return {'result': 'failure', 'reason': 'Could not add new items'}
-    #return {'keys': keys.split('/'), 'example': example}
 
 @route('/batch/<items:path>')
 def batch_add(items):
@@ -151,7 +157,6 @@ def batch_add(items):
     results = {}
     for key in items.split('/'):
         if key:
-            #add_item(key)
             if peon.addItem(key):
                 results[key] = {'result': 'success'}
             else:
@@ -161,13 +166,14 @@ def batch_add(items):
 @route('/get/:key')
 def get_item(key):
     """ Get existing item """
-    return {'found': [item.value for item in Key.objects(value=unicode(key, 'utf-8'))]}
+    return {'found': [
+        item.value for item in Key.objects(value=unicode(key, 'utf-8'))
+        ]}
 
 
 @route('/list/')
 def list_items():
     """ List all items """
-    #return {'items': [item.value for item in Key.objects]}
     return render('list', items=Key.objects())
 
 
