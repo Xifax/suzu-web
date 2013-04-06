a2f268924b2691184a31899fb78a5077e4784d3d
Artiom Basenko <demi.log@gmail.com>
2013-04-06 05:25:28 -0400

    Updated jisho api with method to lookup kanji completion + kana + meaning

--- src/api/jp/jisho.py
+++ src/api/jp/jisho.py
@@ -5,6 +5,8 @@
     Jisho.org semi-api
 """
 
+import re
+
 import requests
 from requests import RequestException
 from bs4 import BeautifulSoup
@@ -35,10 +37,34 @@ class Jisho:
                 
         return results
         
-    def define(self, kanji):
-        """Get words with specified kanji + translations"""
-        pass
+    def define(self, kanji, skip_same_reading = False, skip_same_meaning = False):
+        """Get words with specified kanji + meaning + kana"""
+        results = {}
+        
+        soup = BeautifulSoup(self.lookup(kanji), 'lxml')
+        
+        # Utility function to get specific row|column text
+        get_row = lambda row, column: row.find('td', column).get_text().strip()
+        columns = ['kanji_column', 'kana_column', 'meanings_column']
+        
+        # Find rows with classes 'odd' and 'even'
+        for row in soup.find_all("tr", { "class" : re.compile(r"^(odd|even)$") }):
+            # Skip 'lower' classes
+            if 'lower' in row['class']:
+                continue
+            
+            # Get columns by names
+            word, kana, meaning = [get_row(row, column) for column in columns]
+            
+            # Append to results if not the same kanji
+            if word != kanji:
+                results[word] = {'kana': kana, 'meaning': meaning}
+            
+        # todo: filter results base on flags
+            
+        return results
+            
     
 if __name__ == '__main__':
-    for word in Jisho().complete(u'鍵'):
-        print word
+    for item, value in Jisho().define(u'鍵').items():
+        print item, value
